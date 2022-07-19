7651728a9ccaa0ede452f23289befd01e8b0b91b
Sergio Ferreira <sergiolcf.dev@hotmail.com>
2022-07-19 19:33:06 -0300

    Incluir Projeto de WebApi

---
+++ Slcf_MAPA.WebApi/Controller/HerokuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slcf_MAPA.WebApi.Controller
+{
+    public class HerokuController
+    {
+
+        [HttpGet]
+        
+
+    }
+}
